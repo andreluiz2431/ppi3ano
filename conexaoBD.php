9082b4ed19745027a28e4334bd2f50c803749129
endputz2431 <alm28062001@gmail.com>
2019-11-22 08:34:50 -0300

    editando conexao

--- conexaoBD.php
+++ conexaoBD.php
@@ -1,5 +1,8 @@
 <?php
-$this->pdo = new PDO('mysql:host=localhost;dbname=ppi3ano','root','');
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+$host = 'localhost';
+$bd = 'ppi3ano';
+$user = 'root';
+$senha = '';
+$this->pdo = new PDO('mysql:host='.$host.';dbname='.$bd.'',$user,$senha);
+$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
