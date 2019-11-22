05ce0c491df755b2a400095a95093d32401d994f
endputz2431 <alm28062001@gmail.com>
2019-11-22 09:15:53 -0300

    .

--- forum.php
+++ forum.php
@@ -14,53 +14,7 @@ $forum = new Forum();
         </form>
 
         <?php
-        // postagem
-        if(!empty($_POST['postagem'])){
-            $postagem = $_POST['postagem'];
 
-            $forum->postagem($postagem);
-        }
-
-        // comentario
-        if(!empty($_POST['comentario'])){
-            $comentario = $_POST['comentario'];
-            $idpost = $_POST['id'];
-
-            $forum->comentarPost($idpost, $comentario);
-        }
-
-        // like POST
-        if(isset($_POST['apl'])){
-            $apl = $_POST['apl'];
-
-            $forum->avaliacaoPost($apl);
-        }
-
-        // deslike POST
-        if(isset($_POST['apd'])){
-            $apd = $_POST['apd'];
-
-            $forum->avaliacaoPostN($apd);
-        }
-
-        // like COMENT
-        if(isset($_POST['acl'])){
-            $acl = $_POST['acl'];
-
-            $forum->avaliacaoComent($acl);
-        }
-
-        // deslike COMENT
-        if(isset($_POST['acd'])){
-            $acd = $_POST['acd'];
-
-            $forum->avaliacaoComentN($acd);
-        }
-
-        $idCircuito = 1; // posts e comentarios de qual circuito
-
-        // ver post
-        $forum->vizualizarPost($idCircuito);
         ?>
     </body>
 </html>
