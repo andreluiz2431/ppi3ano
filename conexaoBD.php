2e32ecf236480619d1af8d0fbdca281d0dc33aad
joaopedrordeo <joaopedrordeo@yahoo.com>
2019-11-24 00:59:28 -0300

    apaguei

--- conexaoBD.php
+++ conexaoBD.php
@@ -5,5 +5,5 @@ $user = 'root';
 $senha = '';
 $this->pdo = new PDO('mysql:host='.$host.';dbname='.$bd.'',$user,$senha);
 $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// teste youtube
+
 ?>
