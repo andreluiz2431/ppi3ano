2d24c8b35581cd3dfd7ea00ea5510534b6f1f8f2
joaopedrordeo <45084266+joaopedrordeo@users.noreply.github.com>
2019-11-24 01:08:54 -0300

    apaguei o teste

--- conexaoBD.php
+++ conexaoBD.php
@@ -5,5 +5,4 @@ $user = 'root';
 $senha = '';
 $this->pdo = new PDO('mysql:host='.$host.';dbname='.$bd.'',$user,$senha);
 $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// teste youtube
 ?>
