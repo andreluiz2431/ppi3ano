b1add36bc96d000e3199d78dd003b803eb4d04ce
endputz2431 <alm28062001@gmail.com>
2019-11-25 08:43:37 -0300

    .

--- sb-admin-2/register.php
+++ sb-admin-2/register.php
@@ -1,99 +1,106 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+    <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-  <title>Registrar</title>
+        <title>Registrar</title>
 
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <!-- Custom fonts for this template-->
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+        <!-- Custom styles for this template-->
+        <link href="css/sb-admin-2.min.css" rel="stylesheet">
+        <style>
+            #gradiente{
+                background: rgb(9,0,0);
+                background: linear-gradient(145deg, rgba(9,0,0,1) 0%, rgba(46,19,1,1) 0%, rgba(187,90,5,1) 46%, rgba(246,120,6,1) 81%, rgba(249,162,83,1) 100%, rgba(255,255,255,1) 100%);
+            }
+        </style>
 
-</head>
+    </head>
 
-<body class="bg-gradient-primary">
+    <body id="gradiente">
+        <!-- <body class="bg-gradient-primary"> -->
 
-  <div class="container" style="margin-top: 10%;" align="center">
+        <div class="container" style="margin-top: 10%;" align="center">
 
-    <div class="col-xl-5 col-lg-8 col-md-9" style="margin-top:8%;">
+            <div class="col-xl-5 col-lg-8 col-md-9" style="margin-top:8%;">
 
-    <div class="card o-hidden border-0 shadow-lg my-5">
-      <div class="card-body p-0">
-        <!-- Nested Row within Card Body -->
-        <div class="row justify-content-center">
-          <div></div>
-          <div class="col-lg-12">
-            <div class="p-5">
-              <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">Cria sua conta!</h1>
-              </div>
-              <form class="user" method="POST" action="register.php">
-                <div class="form-group">
-                    <input type="text" class="form-control form-control-user" id="exampleFirstName" title="Seu nome completo" placeholder="Seu nome completo" name="nome">
-                <div class="form-group" style="margin-top: 3.5%;">
-                  <input type="email" class="form-control form-control-user" id="exampleInputEmail" title="Digite seu E-mail" placeholder="Digite seu E-mail" name="email">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row justify-content-center">
+                            <div></div>
+                            <div class="col-lg-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Cria sua conta!</h1>
+                                    </div>
+                                    <form class="user" method="POST" action="register.php">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user" id="exampleFirstName" title="Seu nome completo" placeholder="Seu nome completo" name="nome">
+                                            <div class="form-group" style="margin-top: 3.5%;">
+                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" title="Digite seu E-mail" placeholder="Digite seu E-mail" name="email">
+                                            </div>
+                                            <div class="form-group row">
+                                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" title="Digite sua senha" placeholder="Digite sua senha" name="senha">
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" title="Digite sua senha novamente" placeholder="Verifique a senha" name="senha2">
+                                                </div>
+                                            </div>
+                                            <input type="submit" class="btn btn-primary btn-user btn-block" value="Cadastrar" title="Fazer login">
+
+                                            </form>
+                                        <hr>
+                                        <div class="text-center">
+                                            <a class="small" href="index.php">Já possui uma conta? Login!</a>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group row">
-                  <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" title="Digite sua senha" placeholder="Digite sua senha" name="senha">
-                  </div>
-                  <div class="col-sm-6">
-                    <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" title="Digite sua senha novamente" placeholder="Verifique a senha" name="senha2">
-                  </div>
-                </div>
-                <input type="submit" class="btn btn-primary btn-user btn-block" value="Cadastrar" title="Fazer login">
 
-              </form>
-              <hr>
-              <div class="text-center">
-                <a class="small" href="index.php">Já possui uma conta? Login!</a>
-              </div>
-                </form>
+
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
 
+            <!-- Bootstrap core JavaScript-->
+            <script src="vendor/jquery/jquery.min.js"></script>
+            <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  </div>
+            <!-- Core plugin JavaScript-->
+            <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+            <!-- Custom scripts for all pages-->
+            <script src="js/sb-admin-2.min.js"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+            </body>
 
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+        </html>
+    <?php
+    if(!empty($_POST["nome"])){
 
-</body>
+        if($_POST["senha"] == $_POST["senha2"]){
 
-</html>
-<?php
-if(!empty($_POST["nome"])){
+            include "class_usuario.php";
 
-if($_POST["senha"] == $_POST["senha2"]){
+            $usuario = new Usuario();
 
-include "class_usuario.php";
+            $usuario->nome = $_POST["nome"];
+            $usuario->email = $_POST["email"];
+            $usuario->senha = md5($_POST["senha"]);
 
-$usuario = new Usuario();
-
-$usuario->nome = $_POST["nome"];
-$usuario->email = $_POST["email"];
-$usuario->senha = md5($_POST["senha"]);
-
-$usuario->inserir();
-}
-}
-?>
+            $usuario->inserir();
+        }
+    }
+    ?>
