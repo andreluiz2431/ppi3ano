822ffd539f09a8ec22b42e92b4b470449eecf225
joaopedrordeo <joaopedrordeo@yahoo.com>
2019-11-28 11:41:51 -0200

    estiloo

--- adicionar_resistores.php
+++ adicionar_resistores.php
@@ -10,13 +10,13 @@ $z++;
 <form method="post" action="<?php echo $pagina; ?>.php">
 
     <input name="numr" value="<?php echo $z; ?>" style="display: none">
-    <input type="submit" value="+">
+    <button type="submit" class="btn btn-primary">+</button>
 
 </form>
 
 <?php
 if(!empty($_POST["numr"])){
-    echo '<input type="button" value="-" onclick="history.go(-1)" /><a href="'.$pagina.'.php">Limpar</a>';
+    echo '<button class="btn btn-outline-primary" value="-" onclick="history.go(-1)" >-</button>  <a href="'.$pagina.'.php">Limpar</a>';
 
     $numr = $_POST['numr'];
     $j = 1;
@@ -25,11 +25,12 @@ if(!empty($_POST["numr"])){
 
     while($j <= $numr){
         $z++;
-        echo "<label>Resistor $j</label><input type='text' name='res_$j' placeholder='Dê um valor ao resistor'><br>";
+        echo "<label>Resistor $j</label><input class='form-control' type='text' name='res_$j' placeholder='Dê um valor ao resistor'><br>";
         $j++;
     }
+
     echo "<input type='number' value='$numr' name='cont' style='display:none'><br>";
-    echo "<input type='submit'></form>";
+    echo "<button type='submit' class='btn btn-primary'>Enviar</button></form>";
 }
 
 if(!empty($_POST['cont'])){
--- circuitos/serie.php
+++ circuitos/serie.php
@@ -1,18 +1,61 @@
 <html>
-    <head>
-        <title>Circuito série</title>
+   <head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+
+
+        <!-- Font Awesome Icons -->
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+        <!-- Plugin CSS -->
+        <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+        <!-- Theme CSS - Includes Bootstrap -->
+        <link href="css/creative.min.css" rel="stylesheet">
+
+        <style>
+            .navbarForum{
+                background-color: #F47E1A;
+                height: 70px;
+            }
+            #imgCircuitos{
+                width: 450px;
+                height: 250px;
+            }
+
+        </style>
     </head>
     <body>
-        <label>Adicione resistores para seu circuito série: </label>
+        <div class="navbarForum">
+            <?php
+            include '../navbar.php';
+            ?>
+        </div>
+        <div class="container-fluid">
+            <div class="row" >
+                <div class="col-md-4">
+                </div>
+                <div class="col-md-4" >
+                    <div style="margin-top: 20%;">
+                        <div class="shadow p-3 mb-5 bg-white rounded">
+                         <h3 class="text-primary"><center>Adicione resistores para seu circuito série: </center></h3>
+
         <?php
         $pagina = 'serie';
-        
+
         include '../adicionar_resistores.php';
-        
+
         include '../classes/class_calculadoras.php';
-        
+
         $calculadora = new Calculos();
-        
+
         if(!empty($c)){
             echo 'Resistência equivalente: '.$calculadora->resistenciaEquivalenteSerie($c);
         }
