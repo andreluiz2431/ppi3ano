85eebb2a25a8c1ad94c7a214394dd74121f1a8ad
joaopedrordeo <joaopedrordeo@yahoo.com>
2019-11-28 12:02:14 -0200

    estilo

--- circuitos/serie.php
+++ circuitos/serie.php
@@ -65,6 +65,24 @@
                 </div>
             </div>
         </div>
+        <!-- Footer -->
+            <footer class="bg-light py-5">
+                <div class="container">
+                    <div class="small text-center text-muted">Copyright &copy; 2019 - FarPhysic</div>
+                </div>
+            </footer>
+
+            <!-- Bootstrap core JavaScript -->
+            <script src="creative/vendor/jquery/jquery.min.js"></script>
+            <script src="creative/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+            <!-- Plugin JavaScript -->
+            <script src="creative/vendor/jquery-easing/jquery.easing.min.js"></script>
+            <script src="creative/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+            <!-- Custom scripts for this template -->
+            <script src="js/creative.min.js"></script>
+
 
     </body>
 </html>
